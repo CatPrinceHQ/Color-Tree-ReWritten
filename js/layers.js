cda6a6a2b87be089aaecea5812f838efb6468773
CatPrinceHQ <146359637+CatPrinceHQ@users.noreply.github.com>
2023-09-28 10:35:19 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -228,4 +228,95 @@ addLayer("r", {
     ],
     layerShown(){return player.w.unlocked}
 })
-*/
+
+
+addLayer("g", {
+    name: "green", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "G", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    branches: ['w'],
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#66ff00",
+    requires: new Decimal(100000), // Can be a function that takes requirement increases into account
+    resource: "Red photons", // Name of prestige currency
+    baseResource: "particles", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 1/4, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    effect() {
+        return new Decimal(1)
+    },
+    effectDescription() { // Optional text to describe the effects
+        
+    },
+    upgrades: {
+
+    },
+    milestones: {
+
+    },
+    buyables: {
+
+    },
+    hotkeys: [
+        {key: "r", description: "R: Reset for red photons", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return player.w.unlocked}
+})
+
+
+addLayer("b", {
+    name: "blue", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "B", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    branches: ['w'],
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#0096FF",
+    requires: new Decimal(100000), // Can be a function that takes requirement increases into account
+    resource: "Red photons", // Name of prestige currency
+    baseResource: "particles", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 1/4, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    effect() {
+        return new Decimal(1)
+    },
+    effectDescription() { // Optional text to describe the effects
+        
+    },
+    upgrades: {
+
+    },
+    milestones: {
+
+    },
+    buyables: {
+
+    },
+    hotkeys: [
+        {key: "r", description: "R: Reset for red photons", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return player.w.unlocked}
+})
